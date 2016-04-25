c9826dfc2115c0284e50db435b47a87ea2afbb88
Ramesh Sambandan <rsamban@gmail.com>
2016-04-25 13:59:43 -0600

    test

--- ci/build-artifact.sh
+++ ci/build-artifact.sh
@@ -4,6 +4,6 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd music-repo
-#echo $version
+echo $version
 gradle assemble -PVersion=$version
-#ls build/libs/
+ls build/libs/
--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
-
+echo "RAMESH..."
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd music-repo
-#echo $version
+echo $version
 gradle assemble -PVersion=$version
-#ls build/libs/
+ls build/libs/
